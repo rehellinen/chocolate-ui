a27e26af22a24e348c0914232b9dcd5e3954be9d
rehellinen <rehellinen@foxmail.com>
2020-04-03 11:02:19 +0800

    feat: 更新示例

--- src/components/cover-selection/cover-selection.vue
+++ src/components/cover-selection/cover-selection.vue
@@ -1,12 +1,36 @@
 <template lang="pug">
-  .cover-selection 待开发
+  .cover-selection
+    button.open-button(@click="openDialog") 点击打开
+    my-dialog(:visible.sync="visible")
+      p 123123
 </template>
 
 <script>
+import MyDialog from '../dialog/dialog'
+
 export default {
+  components: {
+    MyDialog
+  },
+  data () {
+    return {
+      visible: true
+    }
+  },
+  methods: {
+    openDialog () {
+      this.visible = true
+    }
+  }
 }
 </script>
 
 <style lang="sass" scoped>
   @import "~sass/base"
+
+  .cover-selection
+    padding: 10px 20px
+    .open-button
+      @include button()
+  .de
 </style>
