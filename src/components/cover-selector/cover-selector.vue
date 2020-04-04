424eabc6e050bdec8f15b494eb9f3dade2404f71
rehellinen <rehellinen@foxmail.com>
2020-04-04 20:49:23 +0800

    feat: 支持滑动选择

--- src/components/cover-selector/cover-selector.vue
+++ src/components/cover-selector/cover-selector.vue
@@ -8,10 +8,17 @@
     )
       .cover-selector__wrapper
         img.cover-selector__cover(:src="images[activeIndex]")
-        .cover-selector__selector
+        .cover-selector__selector(
+          @mousemove="move"
+          @mouseleave="stop"
+          @mouseup="stop"
+        )
           .cover-selector__last(@click="toLast")
-          .cover-selector__thumbnails
-            .cover-selector__selected(:style="selectedStyle")
+          .cover-selector__thumbnails(ref="images")
+            .cover-selector__selected(
+              @mousedown="begin"
+              :style="selectedStyle"
+            )
             img.cover-selector__thumbnail(
               @click="selectImg(i)"
               v-for="(img, i) in images"
@@ -22,6 +29,8 @@
 
 <script>
 import ChocDialog from '../dialog/dialog'
+
+const thumbnailWidth = 40
 
 export default {
   components: {
@@ -39,39 +48,72 @@ export default {
   },
   data () {
     return {
-      activeIndex: this.defaultActiveIndex,
-      visible: true
+      activeIndex: this.defaultActiveIndex, // 已选择的图片Index
+      visible: true, // 是否显示dialog
+      offsetLeft: -2, // 选择框距离最左侧图片的距离
+      moveDistance: 0 // 鼠标拖动选择框的距离
     }
   },
   computed: {
     selectedStyle () {
-      const offsetLeft = this.activeIndex * 40 - 2
-      return `margin-left: ${offsetLeft}px`
+      return `margin-left: ${this.offsetLeft + this.moveDistance}px;`
+    }
+  },
+  watch: {
+    activeIndex (val) {
+      this.offsetLeft = val * thumbnailWidth - 2
     }
   },
   methods: {
-    selectImg (i) {
-      this.activeIndex = i
-    },
     openDialog () {
       this.visible = true
     },
+    // 直接点击图片进行选择
+    selectImg (i) {
+      this.activeIndex = i
+    },
+    // 通过箭头选择上一张照片
     toNext () {
-      if (this.activeIndex === this.images.length - 1) {
-        return
-      }
+      if (this.activeIndex === this.images.length - 1) return
       this.activeIndex++
     },
+    // 通过箭头选择下一张照片
     toLast () {
-      if (this.activeIndex === 0) {
-        return
-      }
+      if (this.activeIndex === 0) return
       this.activeIndex--
     },
     toConfirm () {
       this.$emit('confirm', {
         url: this.images[this.activeIndex]
       })
+    },
+    // 以下为实现可拖拽选择框的四个事件处理函数
+    begin (e) {
+      this.initialDistance = e.clientX
+    },
+    move (e) {
+      if (this.initialDistance) {
+        const maxLeft = this.activeIndex * thumbnailWidth
+        const maxRight = (this.images.length - this.activeIndex - 1) * thumbnailWidth
+        const moveDistance = e.clientX - this.initialDistance
+
+        // 判断选择框是否溢出
+        if (moveDistance > maxRight) {
+          this.moveDistance = maxRight
+        } else if (moveDistance < 0 && -moveDistance > maxLeft) {
+          this.moveDistance = -maxLeft
+        } else {
+          this.moveDistance = moveDistance
+        }
+      }
+    },
+    stop (e) {
+      if (this.initialDistance) {
+        const allLeft = this.offsetLeft + this.moveDistance
+        this.activeIndex = Math.round(allLeft / thumbnailWidth)
+        this.initialDistance = 0
+        this.moveDistance = 0
+      }
     }
   }
 }
@@ -108,7 +150,6 @@ export default {
         border: 2px solid white
         position: absolute
         margin-top: -2px
-        transition: all 0.5s ease-in-out
         cursor: pointer
       .cover-selector__thumbnails
         .cover-selector__thumbnail
