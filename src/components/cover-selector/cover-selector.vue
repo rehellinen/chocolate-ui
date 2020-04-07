1be0ba056b2c25f54a9356501fcd24ff8c70560f
rehellinen <rehellinen@foxmail.com>
2020-04-07 16:20:13 +0800

    refactor: 不换行版本的图片滑动选择器

--- src/components/cover-selector/cover-selector.vue
+++ src/components/cover-selector/cover-selector.vue
@@ -14,24 +14,33 @@
           @mouseup="stop"
         )
           .cover-selector__last(@click="toLast")
-          .cover-selector__thumbnails(ref="images")
+          .cover-selector__thumbnails-wrapper
             .cover-selector__selected(
               @mousedown="begin"
               :style="selectedStyle"
               :class="{ transition }"
             )
-            img.cover-selector__thumbnail(
-              @click="selectImg(i)"
-              v-for="(img, i) in images"
-              :src="img"
+            .cover-selector__thumbnails(
+              :style="imagesStyle"
+              ref="images"
             )
+              img.cover-selector__thumbnail(
+                @load="initDistance"
+                @click="selectImg(i)"
+                v-for="(img, i) in images"
+                :src="img"
+              )
           .cover-selector__next(@click="toNext")
 </template>
 
 <script>
 import ChocDialog from '../dialog/dialog'
 
-const thumbnailWidth = 40
+let thumbnailWidth = 40
+let completedCount = 0 // 完成加载的图片数量
+const distances = [] // 缓存每张图片到最左侧的距离以增加性能
+const selectedTotalWidth = 430 // 选择框总长度为430px
+const selectedBorder = 2 // 选择框的border宽度
 
 export default {
   components: {
@@ -51,22 +60,62 @@ export default {
     return {
       activeIndex: this.defaultActiveIndex, // 已选择的图片Index
       visible: true, // 是否显示dialog
+      selectedWidth: 0, // 选择框的长度
       transition: true, // 是否开启选择框的动画
-      offsetLeft: -2, // 选择框距离最左侧图片的距离
-      moveDistance: 0 // 鼠标拖动选择框的距离
+      selectedOffset: 0, // 选择框偏移距离
+      imagesOffset: 0 // 缩略图偏移距离
     }
   },
   computed: {
     selectedStyle () {
-      return `margin-left: ${this.offsetLeft + this.moveDistance}px;`
+      return `margin-left: ${this.selectedOffset - selectedBorder}px; width: ${this.selectedWidth}px`
+    },
+    imagesStyle () {
+      return `transform: translateX(-${this.imagesOffset}px)`
     }
   },
   watch: {
-    activeIndex (val) {
-      this.offsetLeft = val * thumbnailWidth - 2
+    activeIndex (newIndex) {
+      this.selectedWidth = distances[newIndex].width
+      const originOffsetLeft = distances[newIndex].offset
+
+      if (originOffsetLeft + this.selectedWidth > selectedTotalWidth) {
+        this.selectedOffset = selectedTotalWidth - this.selectedWidth
+        this.imagesOffset = originOffsetLeft - this.selectedOffset
+      } if (originOffsetLeft <= 0) {
+        this.selectedOffset = 0
+        this.imagesOffset = 0
+      } else {
+        this.selectedOffset = originOffsetLeft - this.imagesOffset
+      }
     }
   },
   methods: {
+    initDistance () {
+      const initInstances = () => {
+        if (distances.length > 0) return
+
+        distances.push({
+          offset: 0,
+          width: 0
+        })
+        const images = [...this.$refs['images'].children]
+        for (let [index, image] of images.entries()) {
+          distances.push({
+            offset: distances[index].offset + image.width,
+            width: 0
+          })
+          distances[index].width = image.width
+          image.width = distances[index].width
+        }
+      }
+      completedCount++
+      if (completedCount === this.images.length) {
+        completedCount = 0
+        initInstances()
+        this.selectedWidth = distances[0].width
+      }
+    },
     openDialog () {
       this.visible = true
     },
@@ -74,12 +123,12 @@ export default {
     selectImg (i) {
       this.activeIndex = i
     },
-    // 通过箭头选择上一张照片
+    // 通过箭头选择下一张照片
     toNext () {
       if (this.activeIndex === this.images.length - 1) return
       this.activeIndex++
     },
-    // 通过箭头选择下一张照片
+    // 通过箭头选择上一张照片
     toLast () {
       if (this.activeIndex === 0) return
       this.activeIndex--
@@ -133,8 +182,7 @@ export default {
   .cover-selector__wrapper
     text-align: center
     .cover-selector__cover
-      width: 200px
-      height: 300px
+      height: 100px
     .cover-selector__selector
       display: flex
       justify-content: space-between
@@ -155,11 +203,17 @@ export default {
         position: absolute
         margin-top: -2px
         cursor: pointer
+        z-index: 100
+        border-radius: 3px
         &.transition
-          transition: margin-left 0.4s ease-in-out
+          transition: all 0.4s ease-in-out
+      .cover-selector__thumbnails-wrapper
+        overflow: hidden
+        max-width: 430px
       .cover-selector__thumbnails
+        display: flex
+        transition: all 0.4s ease-in-out
         .cover-selector__thumbnail
-          width: 40px
           height: 60px
           vertical-align: bottom
           cursor: pointer
