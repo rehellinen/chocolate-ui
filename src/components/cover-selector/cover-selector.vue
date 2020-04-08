ef47f66aad89ad0d91029a42e1f3636eb947dd4e
rehellinen <rehellinen@foxmail.com>
2020-04-08 10:38:08 +0800

    feat: 支持拖拽选择

--- src/components/cover-selector/cover-selector.vue
+++ src/components/cover-selector/cover-selector.vue
@@ -9,6 +9,7 @@
       .cover-selector__wrapper
         img.cover-selector__cover(:src="images[activeIndex]")
         .cover-selector__selector(
+          @mousedown="begin"
           @mousemove="move"
           @mouseleave="stop"
           @mouseup="stop"
@@ -16,15 +17,17 @@
           .cover-selector__last(@click="toLast")
           .cover-selector__thumbnails-wrapper
             .cover-selector__selected(
-              @mousedown="begin"
               :style="selectedStyle"
               :class="{ transition }"
             )
             .cover-selector__thumbnails(
               :style="imagesStyle"
+              :class="{ transition }"
               ref="images"
             )
               img.cover-selector__thumbnail(
+                @dragstart.prevent="noop"
+                @selectstart.prevent="noop"
                 @load="initDistance"
                 @click="selectImg(i)"
                 v-for="(img, i) in images"
@@ -36,7 +39,6 @@
 <script>
 import ChocDialog from '../dialog/dialog'
 
-let thumbnailWidth = 40
 let completedCount = 0 // 完成加载的图片数量
 const distances = [] // 缓存每张图片到最左侧的距离以增加性能
 const selectedTotalWidth = 430 // 选择框总长度为430px
@@ -63,7 +65,8 @@ export default {
       selectedWidth: 0, // 选择框的长度
       transition: true, // 是否开启选择框的动画
       selectedOffset: 0, // 选择框偏移距离
-      imagesOffset: 0 // 缩略图偏移距离
+      imagesOffset: 0, // 缩略图偏移距离
+      moveDistance: 0 // 用户移动鼠标的水平距离
     }
   },
   computed: {
@@ -71,26 +74,30 @@ export default {
       return `margin-left: ${this.selectedOffset - selectedBorder}px; width: ${this.selectedWidth}px`
     },
     imagesStyle () {
-      return `transform: translateX(-${this.imagesOffset}px)`
+      return `transform: translateX(${-(this.imagesOffset - this.moveDistance)}px)`
     }
   },
   watch: {
     activeIndex (newIndex) {
+      this.computeSelected(newIndex)
+    }
+  },
+  methods: {
+    computeSelected (newIndex) {
       this.selectedWidth = distances[newIndex].width
       const originOffsetLeft = distances[newIndex].offset
 
       if (originOffsetLeft + this.selectedWidth > selectedTotalWidth) {
         this.selectedOffset = selectedTotalWidth - this.selectedWidth
         this.imagesOffset = originOffsetLeft - this.selectedOffset
-      } if (originOffsetLeft <= 0) {
+      } if (originOffsetLeft - this.imagesOffset <= 0) {
         this.selectedOffset = 0
-        this.imagesOffset = 0
+        this.imagesOffset = originOffsetLeft
       } else {
         this.selectedOffset = originOffsetLeft - this.imagesOffset
       }
-    }
-  },
-  methods: {
+    },
+    noop () {},
     initDistance () {
       const initInstances = () => {
         if (distances.length > 0) return
@@ -121,7 +128,7 @@ export default {
     },
     // 直接点击图片进行选择
     selectImg (i) {
-      this.activeIndex = i
+      // this.activeIndex = i
     },
     // 通过箭头选择下一张照片
     toNext () {
@@ -141,19 +148,18 @@ export default {
     // 以下为实现可拖拽选择框的四个事件处理函数
     begin (e) {
       this.transition = false
-      this.initialDistance = e.clientX
+      this.initialDistance = e.screenX
     },
     move (e) {
       if (this.initialDistance) {
-        const maxLeft = this.activeIndex * thumbnailWidth
-        const maxRight = (this.images.length - this.activeIndex - 1) * thumbnailWidth
-        const moveDistance = e.clientX - this.initialDistance
+        const maxRight = distances[(this.images.length)].offset - selectedTotalWidth
+        const moveDistance = e.screenX - this.initialDistance
 
         // 判断选择框是否溢出
-        if (moveDistance > maxRight) {
-          this.moveDistance = maxRight
-        } else if (moveDistance < 0 && -moveDistance > maxLeft) {
-          this.moveDistance = -maxLeft
+        if (moveDistance < 0 && this.imagesOffset - moveDistance > maxRight) {
+          this.moveDistance = this.imagesOffset - maxRight
+        } else if (moveDistance > 0 && this.imagesOffset - moveDistance < 0) {
+          this.moveDistance = this.imagesOffset
         } else {
           this.moveDistance = moveDistance
         }
@@ -162,10 +168,31 @@ export default {
     stop (e) {
       if (this.initialDistance) {
         this.transition = true
-        const allLeft = this.offsetLeft + this.moveDistance
-        this.activeIndex = Math.round(allLeft / thumbnailWidth)
         this.initialDistance = 0
+
+        this.imagesOffset -= this.moveDistance
         this.moveDistance = 0
+        let rightIndex
+        for (let [index, item] of distances.entries()) {
+          if (item.offset > this.imagesOffset + this.selectedOffset) {
+            rightIndex = index
+            break
+          }
+        }
+        const rightOffset = distances[rightIndex].offset - this.imagesOffset
+        const leftOffset = distances[rightIndex - 1].offset - this.imagesOffset
+        if (rightOffset - this.selectedOffset > this.selectedOffset - leftOffset) {
+          if (this.activeIndex === rightIndex - 1) {
+            this.computeSelected(this.activeIndex)
+          } else {
+            this.activeIndex = rightIndex - 1
+          }
+        } else {
+          if (this.activeIndex === rightIndex) {
+            this.computeSelected(this.activeIndex)
+          }
+          this.activeIndex = rightIndex
+        }
       }
     }
   }
@@ -196,26 +223,27 @@ export default {
         @include arrow(45deg)
       .cover-selector__next
         @include arrow(-135deg)
+      .cover-selector__thumbnails-wrapper
+        overflow: hidden
+        max-width: 430px
       .cover-selector__selected
         width: 40px
         height: 60px
         border: 2px solid white
         position: absolute
         margin-top: -2px
-        cursor: pointer
+        /*cursor: pointer*/
         z-index: 100
         border-radius: 3px
         &.transition
-          transition: all 0.4s ease-in-out
-      .cover-selector__thumbnails-wrapper
-        overflow: hidden
-        max-width: 430px
+          transition: all 0.3s ease-in-out
       .cover-selector__thumbnails
         display: flex
-        transition: all 0.4s ease-in-out
+        &.transition
+          transition: all 0.3s ease-in-out
         .cover-selector__thumbnail
           height: 60px
           vertical-align: bottom
-          cursor: pointer
+          /*cursor: pointer*/
   .de
 </style>
